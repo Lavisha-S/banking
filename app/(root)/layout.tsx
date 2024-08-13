3a7c2b029c78bc68c1f5d984fd95a93f259d4c3f
Lavisha S <work.lavishaa@gmail.com>
2024-08-13 16:16:15 +0530

    ok

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,19 @@
 
+import Sidebar from "@/components/ui/Sidebar"
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn={firstName:'Lavisha',lastName:'S'};
   return (
-    <html lang="en">
-      <main>SIDEBAR
+    
+      <main className="flex h-screen w-full font-inter">
+        <Sidebar
+        user={loggedIn}
+        />
         {children}
       </main>
-    </html>
+    
   );
 }
